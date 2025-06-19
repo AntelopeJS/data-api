d3c338e98c34cbfcd63639a5c46cae10ff3eb462
Thomasims <thomas@antelopejs.com>
2025-06-19 11:31:42 +0200

    fix: prettier inconsistency

--- src/interfaces/data-api/beta/index.ts
+++ src/interfaces/data-api/beta/index.ts
@@ -22,8 +22,8 @@ export type DataControllerCallbackWithOptions<O = any> = {
 export type ExtractCallback<T> = T extends DataControllerCallbackWithOptions
   ? T['callback']['func']
   : T extends DataControllerCallback
-  ? T['func']
-  : never;
+    ? T['func']
+    : never;
 
 export type DataControllerDef = {
   [name: string]: DataControllerCallback | DataControllerCallbackWithOptions;

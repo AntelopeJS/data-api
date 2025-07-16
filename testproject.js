6ebf7a4c37e5ffa70e4394ff8869c02a85cc37e9
glastis <glastis@glastis.com>
2025-07-16 11:23:33 +0200

    chore: tests module install command

--- testproject.js
+++ testproject.js
@@ -2,55 +2,57 @@ const { MongoMemoryServer } = require('mongodb-memory-server-core');
 
 let mongod;
 
-module.exports.setup = async function() {
-	mongod = await MongoMemoryServer.create();
+module.exports.setup = async function () {
+  mongod = await MongoMemoryServer.create();
 
-	return {
-		cacheFolder: '.antelope/cache',
-		modules: {
-			local: {
-				source: {
-					type: 'local',
-					path: '.',
-				},
-			},
-			mongodb: {
-				source: {
-					type: 'git',
-					remote: 'git@github.com:AntelopeJS/mongodb.git',
-					branch: 'main',
-				},
-				config: {
-					url: mongod.getUri(),
-				},
-			},
-      		database_decorators: {
-				source: {
-					type: 'git',
-					remote: 'git@github.com:AntelopeJS/database-decorators.git',
-					branch: 'main',
-				},
-			},
-			api: {
-				source: {
-					type: 'git',
-					remote: 'git@github.com:AntelopeJS/api.git',
-					branch: 'main',
-					installCommand: ['pnpm i', 'npx tsc'],
-				},
-				config: {
-					servers: [
-						{
-							protocol: 'http',
-							port: '5010',
-						},
-					],
-				},
-			},
-		},
-	};
+  return {
+    cacheFolder: '.antelope/cache',
+    modules: {
+      local: {
+        source: {
+          type: 'local',
+          path: '.',
+        },
+      },
+      mongodb: {
+        source: {
+          type: 'git',
+          remote: 'git@github.com:AntelopeJS/mongodb.git',
+          branch: 'main',
+          installCommand: ['pnpm i', 'npx tsc'],
+        },
+        config: {
+          url: mongod.getUri(),
+        },
+      },
+      database_decorators: {
+        source: {
+          type: 'git',
+          remote: 'git@github.com:AntelopeJS/database-decorators.git',
+          branch: 'main',
+          installCommand: ['pnpm i', 'npx tsc'],
+        },
+      },
+      api: {
+        source: {
+          type: 'git',
+          remote: 'git@github.com:AntelopeJS/api.git',
+          branch: 'main',
+          installCommand: ['pnpm i', 'npx tsc'],
+        },
+        config: {
+          servers: [
+            {
+              protocol: 'http',
+              port: '5010',
+            },
+          ],
+        },
+      },
+    },
+  };
 };
 
-module.exports.cleanup = async function() {
-	await mongod.stop();
+module.exports.cleanup = async function () {
+  await mongod.stop();
 };
